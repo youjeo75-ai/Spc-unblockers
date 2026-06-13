3000de219f6da05cba39139bc6506dd9b6ad13f6
youjeo75-ai <youjeo75@gmail.com>
2026-06-13 13:23:12 +0200

    Add files via upload

---
+++ games.js
@@ -0,0 +1,214 @@
+
+const gamesDatabase = [
+    {
+        "id": "supermario-64",
+        "title": "Supermario 64",
+        "category": "Action",
+        "image": "https://tse3.mm.bing.net/th/id/OIP.fDndcD_K7ueTuqS84T9OXAAAAA?w=300&h=300&rs=1&pid=ImgDetMain&o=7&rm=3",
+        "url": "https://classicjoy.games/embed?slug=super-mario-64",
+        "description": "Super mario 64",
+        "imageGradient": "linear-gradient(135deg, #06060aff, #1f1f2e)"
+    },
+    
+
+    
+     {
+        "id": "minecraft",
+        "title": "Minecraft",
+        "category": "Casual",
+        "image": "https://static.wikia.nocookie.net/minecraft/images/9/94/MSPromotionalBackground.jpg/revision/latest?cb=20190113141352",
+        "url": "https://eaglercraft.ir/play/1.8.8?touch=false&userscript=",
+        "description": "Minecraft",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+   
+    {
+        "id": "smashkarts",
+        "title": "Smash Karts",
+        "category": "Driving",
+        "image": "https://img.poki-cdn.com/cdn-cgi/image/q=78,scq=50,width=1200,height=1200,fit=cover,f=png/9c9e529b14731be871b07b89660bbc2a/smash-karts.png",
+        "url": "https://smash-karts.com/",
+        "description": "Smash Karts",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+
+    {
+        "id": "1v1lol",
+        "title": "1v1lol",
+        "category": "Action",
+        "image": "https://images.crazygames.com/games/1v1-lol/cover-1585728351086.png?auto=format%2Ccompress&q=45&cs=strip&ch=DPR&w=1200&h=630&fit=crop",
+        "url": "https://1v1s.lol/",
+        "description": "1v1lol",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+
+     {
+        "id": "minecraft(modded)",
+        "title": "Minecraft(modded)",
+        "category": "Casual",
+        "image": "https://static.wikia.nocookie.net/minecraft/images/9/94/MSPromotionalBackground.jpg/revision/latest?cb=20190113141352",
+        "url": "https://mcraft.fun/",
+        "description": "Minecraft(modded)",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+
+    {
+        "id": "slope",
+        "title": "Slope",
+        "category": "Action",
+        "image": "https://unblocked-game77.com/wp-content/uploads/2024/03/slope.jpg",
+        "url": "https://slope.lol/",
+        "description": "Slope",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "openfront",
+        "title": "Openfront",
+        "category": "Action",
+        "image": "https://iogamesonly.com/media/openfront-io.png",
+        "url": "https://frontwars.io/",
+        "description": "Openfront",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "spacewaves",
+        "title": "Spacewaves",
+        "category": "Multiplayer",
+        "image": "https://tse1.mm.bing.net/th/id/OIP.AsxM2u57ZyVDwrjrE1fEegHaEK?rs=1&pid=ImgDetMain&o=7&rm=3",
+        "url": "https://spacewaves.io/",
+        "description": "Spacewaves",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    
+    {
+        "id": "Mario Kart 64",
+        "title": "Mario Kart 64",
+        "category": "Driving",
+        "image": "https://tse1.mm.bing.net/th/id/OIP.71E8wR0fbTad_ZnXWo7EvQHaJ4?w=810&h=1080&rs=1&pid=ImgDetMain&o=7&rm=3",
+        "url": "https://classicjoy.games/embed?slug=mario-kart-64" ,
+        "description": "Mario Kart 64",
+        "imageGradient": "linear-gradient(135deg, #ffe600ff, #ff0000ff)"
+    },
+    {
+        "id": "paper-io",
+        "title": "paper.io",
+        "category": "Multiplayer",
+        "image": "https://imgs2.dab3games.com/paperio2alt.png",
+        "url": "https://paper.io",
+        "description": "paper.io",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "veck",
+        "title": "Veck.io",
+        "category": "Multiplayer",
+        "image": "https://tse3.mm.bing.net/th/id/OIP.l2mprc_zGl8O8qaDdokCWQHaHa?rs=1&pid=ImgDetMain&o=7&rm=3",
+        "url": "https://veck.io",
+        "description": "Veck.io",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "Roblox",
+        "title": "Roblox",
+        "category": "Multiplayer",
+        "image": "https://th.bing.com/th/id/OIP.zGGg3rj0HAgJpDoJOHAa8wHaD4?o=7rm=3&rs=1&pid=ImgDetMain&o=7&rm=3",
+        "url": "https://bloxgames.io/",
+        "description": "Roblox",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "polytrack",
+        "title": "Polytrack",
+        "category": "Multiplayer",
+        "image": "https://watchdocumentaries.com/wp-content/uploads/polytrack-game.jpg",
+        "url": "https://polytrackmodded.github.io/",
+        "description": "Polytrack",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "slowroads",
+        "title": "Slowroads",
+        "category": "Multiplayer",
+        "image": "https://www.gaming.net/wp-content/uploads/2025/02/Your-paragraph-text-2025-02-21T105128.543.jpg",
+        "url": "https://slowroads.io/",
+        "description": "Slowroads",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "worldguessr",
+        "title": "Worldguessr",
+        "category": "Multiplayer",
+        "image": "https://worldguesser.io/data/image/world-guesser-banner.png",
+        "url": "https://www.worldguessr.com/",
+        "description": "Worldguessr",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "colorrush",
+        "title": "Colorrush",
+        "category": "Multiplayer",
+        "image": "https://colorrush.org/data/image/how-to-play-color-rush-crorg.png",
+        "url": "https://colorrush.org/game/",
+        "description": "Colorrush",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "curverush",
+        "title": "Curverush",
+        "category": "Multiplayer",
+        "image": "https://azgames.io/upload/cache/upload/imgs/curverush2-m180x180.png",
+        "url": "https://curverush.com/",
+        "description": "Curverush",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+    {
+        "id": "sloperider",
+        "title": "Sloperider",
+        "category": "Action",
+        "image": "https://azgames.io/upload/cache/upload/imgs/sloperider4-m180x180.png",
+        "url": "https://sloperider2.github.io/",
+        "description": "Sloperider",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+    },
+      
+      {
+        "id": "climb-it-up",
+        "title": "Climb It Up",
+        "category": "Action",
+        "image": "https://agaronline.io/cache/data/image/game/climb-it-up-m110x70.png",
+        "url": "https://agaronline.io/climb-it-up/fullscreen#google_vignette",
+        "description": "Climb It Up",
+        "imageGradient": " linear-gradient(135deg, #000000ff, #1f1f2e)"
+      },
+
+     
+      
+      
+      
+
+    {
+        "id": "youtube",
+        "title": "YouTube",
+        "category": "Apps",
+        "image": "https://play-lh.googleusercontent.com/QNmuZQc9I6Zbe3mWnSr0hycnENqGFCI5p3yE29Hkxtf22T0IWS6zTrpxULLyyjWpB7ONAXDsDQXnXcVWokl3eg",
+        "url": "https://613tube.com/",
+        "description": "YouTube - Unblocked via Invidious mirror.",
+        "imageGradient": " linear-gradient(135deg, #ff0000ff, #282828)"
+    },
+
+
+   
+    
+    {
+        "id": "web-proxy",
+        "title": "Web Proxy",
+        "category": "Apps",
+        "image": "",
+        "url": "https://www-proxy.hidester.one/",
+        "description": "Web Proxy - Bypasser for any website. you can use it for discord whatsapp twitch and any website.",
+        "imageGradient": " linear-gradient(135deg, #333333, #000000)"
+    }
+,
+    
+
+];
